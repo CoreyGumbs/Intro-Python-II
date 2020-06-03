6364514b07ef55d38f5fa2f8bce7cb9f40c43f6e
Corey Gumbs <cgumbswebdev@gmail.com>
2020-06-03 17:29:59 -0400

    Added Logic to Game

--- src/player.py
+++ src/player.py
@@ -2,14 +2,30 @@
 # currently.
 class Player:
     def __init__(self, name, room):
-        self.name =  name
-        self.room =  room
-        
+        self.name = name
+        self.room = room
+        self.inventory = []
+    
     def movePlayer(self, room):
         self.room = room
 
     def playerRoom(self):
         return self.room
+
+    def getItem(self, item):  
+        self.inventory.append(item)
+    
+    def dropItem(self, item):
+        self.inventory.remove(item)
+
+    def showInventory(self):
+        print("Player Inventory: \n")
+        if len(self.inventory) >= 1:
+            for i, val in enumerate(self.inventory):
+                print(f"{val.name}: {val.description}")
+        else:
+            print('There is nothing in your inventory')
+        print("\n\n")
 
     def __str__(self):
         return f"{self.name}: {self.room.name}"
